06f12c468ccfa4b11def40a7899d50298036b9f4
Zitong Mao <njsxmzt@gmail.com>
2017-04-19 00:42:33 -0300

    Delete Bagging.R

--- Bagging.R
+++
@@ -1,7 +0,0 @@
-set.seed(1)
-
-flmodel.forest2 <- randomForest(as.factor(onset) ~ warl + gdpenl + 
-                                  lpopl1 + lmtnest + ncontig + 
-                                  Oil + nwstate + instab + polity2l
-                                + ethfrac + relfrac, data = trainingset, mtry = 6, importance = TRUE)
-flmodel.forest2
